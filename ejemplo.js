c52dc370f00aeed64ee186d271cd5868839d23e9
luis782006 <luisfelipe782006@gmail.com>
2022-10-22 10:52:52 -0300

    ejemplos Javascript

--- ejemplo.js
+++ ejemplo.js
@@ -14,18 +14,39 @@
 // }
 // console.log(num);   
 
-const array=[1,9,6,9,3];
-let valorRepetido=array[0];
-let num=0
-let cont=0;
-for (let i = 0; i < array.length; i++) {
-        if (valorRepetido==array[i]) {
-            cont+=1;
-        }
-        if (cont = 2){ 
-            num=array[i];
-        } 
+// const array=[1,9,6,9,3];
+// let valorRepetido=array[0];
+// let num=0
+// let cont=0;
+// for (let i = 0; i < array.length; i++) {
+//         if (valorRepetido==array[i]) {
+//             cont+=1;
+//         }
+//         if (cont = 2){ 
+//             num=array[i];
+//         } 
+//     }
+// console.log(num);  
+
+const personas=[
+    
+        {"nombre":"Julio","edad":17},
+        {"nombre":"Ana","edad":18},
+        {"nombre":"Pepe","edad":25},
+        {"nombre":"Alberto","edad":12},
+        {"nombre":"Noelia","edad":21}
+    
+]
+
+function mayoresDeEdad(personas){
+    let listaDeMayores=[];
+    for (let index = 0; index < personas.length; index++) {
+       
+        if (personas[index].edad>18)
+            listaDeMayores.push(personas[index].nombre)
+        
+       
     }
-console.log(num);   
-
-
+    return listaDeMayores
+}
+console.log(mayoresDeEdad(personas));
---
+++ ejercicio2.js
@@ -0,0 +1,17 @@
+// let persona= [
+//     {nombre:"", apellido:"",dni:0, edad: 0}
+// ];
+
+
+let personaCreada=[];
+
+function crearPersona(nombre,apellido,dni,edad){
+    let persona={
+                nombre:nombre,
+                apellido:apellido,
+                dni:dni,
+                edad:edad};
+    return persona;
+}
+crearPersona("Luis","Sanchez",19096989,43);
+
